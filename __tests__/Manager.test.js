8e768fdd9a72554e3c5f492747cbff7be394d282
CassBut <clhurlbut@gmail.com>
2020-12-20 11:28:23 -0800

    all tests passing and screenshots added

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,21 @@
 const { test, expect } = require("@jest/globals");
 const Manager = require("../lib/Manager");
+
+test("Set Office number", () => {
+    const testOffice = 101;
+    const manager = new Manager("Bill", 1234, "1234@test.com", testOffice);
+    expect(manager.officeNumber).toBe(testOffice);
+});
+
+test("Return Office number", () => {
+    const testOffice = 101;
+    const manager = new Manager("Bill", 1234, "1234@test.com", testOffice);
+    expect(manager.getOfficeNumber()).toBe(testOffice);
+
+});
+
+test("Role is returned as \"Manager\"", () => {
+    const testRole = "Manager";
+    const manager = new Manager("Bill", 1234, "1234@test.com", 101);
+    expect(manager.getRole()).toBe(testRole);
+});
